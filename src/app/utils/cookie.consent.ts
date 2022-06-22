de90bae96e9d63a614640ae5b339b4459e47416a
Ruddy Monlouis <r.monlouis@gmail.com>
2022-06-22 22:18:44 +0200

    fix cookie issue

--- src/app/utils/cookie.consent.ts
+++ src/app/utils/cookie.consent.ts
@@ -1,23 +1,26 @@
 export class CookieConsent {
 
     public static getCookie(name: string): string {
-        let cookies: Array<string> = document.cookie.split(';');
-        for (let cookie of cookies) {
-            let keyValue = cookie.split("=")
-            let key = keyValue[0].trim()
-            let value = keyValue[1].trim()
-            if (key == name) {
-                return value
+        let cookie: string = document.cookie
+        if (cookie) {
+            let cookies: Array<string> = cookie.trim().split(';');
+            for (let cookie of cookies) {
+                let keyValue = cookie.split("=")
+                let key = keyValue[0].trim()
+                let value = keyValue[1].trim()
+                if (key == name) {
+                    return value
+                }
             }
         }
         return ''
     }
 
-    public static deleteCookie(name: string) {
+    public static deleteCookie(name: string): void {
         this.setCookie(name, '', -1);
     }
 
-    public static setCookie(name: string, value: string, expireDays: number, path: string = '') {
+    public static setCookie(name: string, value: string, expireDays: number, path: string = ''): void {
         let date: Date = new Date();
         date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
         let expires: string = `expires=${date.toUTCString()}`;
